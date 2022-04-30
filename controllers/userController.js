49a4513c75d25aef9e3857478c22b2233c47c96f
“Godswilly” <kalu.agukalu@gmail.com>
2022-04-30 10:35:47 +0100

    Add get all users endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,29 @@ exports.createUser = async (req, res, next) => {
 		res.status(400).send(error.message);
 	}
 };
+
+exports.getAllUsers = async (req, res, next) => {
+	try {
+		const users = await admin.collection('users');
+		const data = await users.get();
+		const usersArray = [];
+		if (data.empty) {
+			res.status(400).send('No user record found');
+		} else {
+			data.forEach((doc) => {
+				const user = new User(
+					doc.id,
+					doc.data().name,
+					doc.data().email,
+					doc.data().photo,
+					doc.data().password,
+					doc.data().passwordConfirm
+				);
+				usersArray.push(user);
+			});
+			res.send(usersArray);
+		}
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 router
 	.route('/')
+	.get(userController.getAllUsers)
 	.post(userController.createUser);
+
+module.exports = router;
